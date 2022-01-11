9b08cdd4d6b62fa80763a58a690c476aed2c1ea8
Florian Torres <50424335+torresflo@users.noreply.github.com>
2022-01-11 21:58:04 +0100

    Fix crash when hiding a goal quest

--- Obsidian/scripts/99900 My Scripts/99901 Quests.rb
+++ Obsidian/scripts/99900 My Scripts/99901 Quests.rb
@@ -4,8 +4,8 @@ module PFM
 		# @param quest_id [Integer] the ID of the quest in the database
 		# @param goal_index [Integer] the index of the goal in the goal order
 		def hide_goal(quest_id, goal_index)
-			return if (quest = @active_quests.fetch(quest_id, nil)).nil?
-			quest[:shown][goal_index] = false
+			return unless (quest = active_quest(quest_id))
+			quest.data_set(:goals_visibility, goal_index, false)
 		end
 
 		# Is a quest started ?
